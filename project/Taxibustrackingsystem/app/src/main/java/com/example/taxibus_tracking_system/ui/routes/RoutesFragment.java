4ab89e67307898a1845121c2bfc412f023e7d422
MaksimPashkovskiy <maximajin@gmail.com>
2020-05-27 22:06:19 +0300

    commit 27.05

---
+++ project/Taxibustrackingsystem/app/src/main/java/com/example/taxibus_tracking_system/ui/routes/RoutesFragment.java
@@ -0,0 +1,35 @@
+package com.example.taxibus_tracking_system.ui.routes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.taxibus_tracking_system.R;
+
+public class RoutesFragment extends Fragment {
+
+    private RoutesViewModel routesViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        routesViewModel =
+                ViewModelProviders.of(this).get(RoutesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_routes, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        routesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
