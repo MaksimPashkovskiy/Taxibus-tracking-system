0c45d411b8929c88ca1e9ff9992d685fba4cde95
MaksimPashkovskiy <maximajin@gmail.com>
2020-05-30 13:52:02 +0300

    commit 30.05

--- project/Taxibustrackingsystem/app/src/main/java/com/example/taxibus_tracking_system/ui/map/MapFragment.java
+++ project/Taxibustrackingsystem/app/src/main/java/com/example/taxibus_tracking_system/ui/map/MapFragment.java
@@ -66,84 +66,86 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap mMap) {
 
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                mMap.getUiSettings().setZoomControlsEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-                mMap.getUiSettings().setCompassEnabled(true);
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    mMap.getUiSettings().setZoomControlsEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                    mMap.getUiSettings().setCompassEnabled(true);
 
-                CameraPosition googlePlex = CameraPosition.builder()
-                        .target(new LatLng(46.4775, 30.7326))
-                        .zoom(12)
-                        .bearing(0)
-                        .build();
+                    CameraPosition googlePlex = CameraPosition.builder()
+                            .target(new LatLng(46.4775, 30.7326))
+                            .zoom(12)
+                            .bearing(0)
+                            .build();
 
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10, null);
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10, null);
 
-                // Prompt the user for permission.
-                getLocationPermission();
+                    // Prompt the user for permission.
+                    getLocationPermission();
 
-                // Turn on the My Location layer and the related control on the map.
-                updateLocationUI();
+                    // Turn on the My Location layer and the related control on the map.
+                    updateLocationUI();
 
-                // Get the current location of the device and set the position of the map.
-                getDeviceLocation();
+                    // Get the current location of the device and set the position of the map.
+                    getDeviceLocation();
 
-                Polyline TaxibusRouteNum4 = mMap.addPolyline(new PolylineOptions()
-                        .clickable(true)
-                        .color(0xFF06B80E)
-                        .add(
-                                new LatLng(46.462774, 30.741011),
-                                new LatLng(46.462140, 30.750177),
-                                new LatLng(46.458404, 30.749777),
-                                new LatLng(46.456952, 30.750734),
-                                new LatLng(46.456929, 30.750476),
-                                new LatLng(46.454510, 30.744127),
-                                new LatLng(46.445317, 30.742730),
-                                new LatLng(46.445110, 30.742145),
-                                new LatLng(46.444871, 30.741987),
-                                new LatLng(46.437887, 30.745578),
-                                new LatLng(46.436386, 30.741482),
-                                new LatLng(46.437375, 30.730792),
-                                new LatLng(46.430399, 30.729401),
-                                new LatLng(46.430273, 30.729077),
-                                new LatLng(46.429954, 30.727740),
-                                new LatLng(46.432360, 30.724596),
-                                new LatLng(46.434975, 30.721103),
-                                new LatLng(46.441556, 30.705968),
-                                new LatLng(46.442224, 30.705623),
-                                new LatLng(46.443031, 30.703697),
-                                new LatLng(46.443010, 30.703110),
-                                new LatLng(46.443042, 30.702807),
-                                new LatLng(46.451712, 30.683057),
-                                new LatLng(46.456786, 30.682264),
-                                new LatLng(46.458027, 30.684525),
-                                new LatLng(46.458208, 30.684526),
-                                new LatLng(46.458557, 30.684020),
-                                new LatLng(46.456980, 30.681754),
-                                new LatLng(46.452389, 30.673891),
-                                new LatLng(46.451070, 30.668670),
-                                new LatLng(46.450611, 30.662513),
-                                new LatLng(46.450710, 30.659510),
-                                new LatLng(46.450967, 30.656341),
-                                new LatLng(46.450452, 30.650652),
-                                new LatLng(46.448635, 30.646283),
-                                new LatLng(46.448351, 30.645417),
-                                new LatLng(46.445131, 30.633854),
-                                new LatLng(46.445031, 30.633479),
-                                new LatLng(46.444887, 30.633511),
-                                new LatLng(46.443986, 30.633724),
-                                new LatLng(46.443557, 30.634400),
-                                new LatLng(46.443760, 30.634630),
-                                new LatLng(46.443879, 30.634212),
-                                new LatLng(46.444887, 30.633511)
-                        ));
+                    Polyline TaxibusRouteNum4 = mMap.addPolyline(new PolylineOptions()
+                            .clickable(true)
+                            .color(0xFF06B80E)
+                            .add(
+                                    new LatLng(46.462774, 30.741011),
+                                    new LatLng(46.462140, 30.750177),
+                                    new LatLng(46.458404, 30.749777),
+                                    new LatLng(46.456952, 30.750734),
+                                    new LatLng(46.456929, 30.750476),
+                                    new LatLng(46.454510, 30.744127),
+                                    new LatLng(46.445317, 30.742730),
+                                    new LatLng(46.445110, 30.742145),
+                                    new LatLng(46.444871, 30.741987),
+                                    new LatLng(46.437887, 30.745578),
+                                    new LatLng(46.436386, 30.741482),
+                                    new LatLng(46.437375, 30.730792),
+                                    new LatLng(46.430399, 30.729401),
+                                    new LatLng(46.430273, 30.729077),
+                                    new LatLng(46.429954, 30.727740),
+                                    new LatLng(46.432360, 30.724596),
+                                    new LatLng(46.434975, 30.721103),
+                                    new LatLng(46.441556, 30.705968),
+                                    new LatLng(46.442224, 30.705623),
+                                    new LatLng(46.443031, 30.703697),
+                                    new LatLng(46.443010, 30.703110),
+                                    new LatLng(46.443042, 30.702807),
+                                    new LatLng(46.451712, 30.683057),
+                                    new LatLng(46.456786, 30.682264),
+                                    new LatLng(46.458027, 30.684525),
+                                    new LatLng(46.458208, 30.684526),
+                                    new LatLng(46.458557, 30.684020),
+                                    new LatLng(46.456980, 30.681754),
+                                    new LatLng(46.452389, 30.673891),
+                                    new LatLng(46.451070, 30.668670),
+                                    new LatLng(46.450611, 30.662513),
+                                    new LatLng(46.450710, 30.659510),
+                                    new LatLng(46.450967, 30.656341),
+                                    new LatLng(46.450452, 30.650652),
+                                    new LatLng(46.448635, 30.646283),
+                                    new LatLng(46.448351, 30.645417),
+                                    new LatLng(46.445131, 30.633854),
+                                    new LatLng(46.445031, 30.633479),
+                                    new LatLng(46.444887, 30.633511),
+                                    new LatLng(46.443986, 30.633724),
+                                    new LatLng(46.443557, 30.634400),
+                                    new LatLng(46.443760, 30.634630),
+                                    new LatLng(46.443879, 30.634212),
+                                    new LatLng(46.444887, 30.633511)
+                            ));
 
-            }
-        });
+                }
+            });
+        }
         return view;
     }
 
